3f372758feb89d8f7a6833377e9f6ff8cd18665b
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-07-07 18:09:37 -0300

    Exercicio 2 e correção de bug no html

--- script/controles-marcacao.js
+++ script/controles-marcacao.js
@@ -13,5 +13,35 @@ function mudaVisibilidade() {
 	
 }
 
+function atualizaControles(event) {
+	
+	let marcacaoAlvo = event.currentTarget;
+	let marcacaoAtual = document.querySelector('.selecionada');
+
+	marcacaoAtual.classList.remove('selecionada');
+	marcacaoAlvo.classList.add('selecionada');
+
+    document.querySelector('#x-da-marcacao').value = parseInt(marcacaoAlvo.style.left);
+    document.querySelector('#y-da-marcacao').value = parseInt(marcacaoAlvo.style.top);
+    document.querySelector('#largura-da-marcacao').value = parseInt(marcacaoAlvo.style.width);
+    document.querySelector('#altura-da-marcacao').value = parseInt(marcacaoAlvo.style.height);
+	
+	
+    document.querySelector('#titulo-da-marcacao').value = marcacaoAlvo.dataset.titulo;
+    document.querySelector('#conteudo-da-marcacao').value = marcacaoAlvo.dataset.conteudo;
+    document.querySelector('#cor-da-marcacao').value = marcacaoAlvo.dataset.cor;
+	
+	let formato = marcacaoAlvo.dataset.formato;
+	
+	document.querySelector(`[value="${formato}"]`).checked = true;
+	
+    
+}
+	
+
 visivelQuery.addEventListener('change', mudaVisibilidade);
+
+for (let componente of componentesQuery) {
+	componente.addEventListener('click', atualizaControles);
+}
 
