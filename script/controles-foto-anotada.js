95420aa6f6a7c680ecd337acf41004e7dea89fe0
Beatriz Souza da Silva <beatriz.souza.silva@accenture.com>
2021-07-08 11:01:02 -0300

    Desafio

--- script/controles-foto-anotada.js
+++ script/controles-foto-anotada.js
@@ -5,4 +5,19 @@ function defineFiltro() {
 	fotoQuery.style.filter = filtroQuery.value;
 }
 
+function readImage(event) {
+  file = event.target.files[0];
+  if (file.type && !file.type.startsWith('image/')) {
+    alert('File is not an image.');
+    return;
+  }
+
+  const reader = new FileReader();
+  reader.addEventListener('load', (event) => {
+    fotoQuery.src = event.target.result;
+  });
+  reader.readAsDataURL(file);
+}
+
+document.querySelector('#imagem').addEventListener('change', readImage);
 filtroQuery.addEventListener('change', defineFiltro);
